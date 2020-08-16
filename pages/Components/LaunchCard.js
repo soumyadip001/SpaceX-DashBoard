9321b0090879792077aa812b3871f8ecf5727b9f
Soumyadip <soumyadiphazra@gmail.com>
2020-08-16 22:00:50 +0530

    fixed build errors

--- pages/Components/LaunchCard.js
+++ pages/Components/LaunchCard.js
@@ -2,20 +2,27 @@ import { Card } from 'react-bootstrap';
 
 const LauchCard = (props) => {
     const missionIds = [];
+    let missionName = '';
+    let flightNumber = '';
+    let launchYear = '';
+
     let missionIdsHTML = null;
     let landingSuccess = '';
     let cardimage = null;
     let articleLink = null;
     let launchSuccess = null;
 
-    if (props.data.mission_id) {
+    if (props.data && props.data.mission_id) {
         missionIdsHTML = props.data.mission_id.map(mission => {
             missionIds.push(mission);
             return <li key={'id-'+props.data.mission_id+Math.random()}>{mission}</li>;
-        })
+        });
+        missionName = props.data.mission_name;
+        flightNumber = props.data.flight_number;
+        launchYear = props.data.launch_year;
     }
 
-    if (props.data.rocket && props.data.rocket.first_stage) {
+    if (props.data && props.data.rocket && props.data.rocket.first_stage) {
         const rocketName = props.data.rocket.rocket_name ? props.data.rocket.rocket_name : 'ROCKET';
         let landingStatus = 'FAIL';
         if (props.data.rocket.first_stage.cores.length > 0) {
@@ -24,17 +31,17 @@ const LauchCard = (props) => {
         landingSuccess = `${rocketName} - ${landingStatus}`;
     }
 
-    if (props.data.links && props.data.links.mission_patch_small) {
+    if (props.data && props.data.links && props.data.links.mission_patch_small) {
         cardimage = <Card.Img variant='top' src={props.data.links.mission_patch_small} />;
     } else {
         cardimage = <Card.Img variant='top' src='https://via.placeholder.com/150' />;
     }
 
-    if (props.data.links && props.data.links.article_link) {
+    if (props.data && props.data.links && props.data.links.article_link) {
         articleLink = <a href={props.data.links.article_link} target="_blank">More Info</a>;
     }
 
-    if (props.data.launch_success) {
+    if (props.data && props.data.launch_success) {
         launchSuccess = <Card.Text>Successful launch: <span className='text-success'>SUCCESS</span></Card.Text>
     } else {
         launchSuccess = <Card.Text>Successful launch: <span className='text-danger'>FAILURE</span></Card.Text>
@@ -44,10 +51,10 @@ const LauchCard = (props) => {
         <Card className='mb-5' style={{ width: '15rem' }}>
             {cardimage}
             <Card.Body>
-                <Card.Title style={{color: '#0d47a1'}}>{props.data.mission_name} #{props.data.flight_number}</Card.Title>
+                <Card.Title style={{color: '#0d47a1'}}>{missionName} #{flightNumber}</Card.Title>
                 <Card.Text><b>Mission Ids:</b></Card.Text>
                 <ul>{missionIdsHTML}</ul>
-                <Card.Text>Successful Year: {props.data.launch_year}</Card.Text>
+                <Card.Text>Successful Year: {launchYear}</Card.Text>
                 {launchSuccess}
                 <Card.Text>Successful landing: {landingSuccess}</Card.Text>
                 <Card.Text>{articleLink}</Card.Text>
